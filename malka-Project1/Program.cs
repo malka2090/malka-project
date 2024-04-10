f23e07881376414a1860af7fb0d519ac7f78f752
malka2090 <elhanan2090@gmail.com>
2024-04-10 16:14:47 +0300

    first commit

---
+++ malka-Project1/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace malka_Project
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
